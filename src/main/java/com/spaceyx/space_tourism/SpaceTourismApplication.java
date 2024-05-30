56f6b30427a46a6a87e1d8d89d15aa8eaf37209d
Carl Kone <carl.kone-takuefou@epita.fr>
2024-05-30 18:48:27 +0200

    initial commit

---
+++ src/main/java/com/spaceyx/space_tourism/SpaceTourismApplication.java
@@ -0,0 +1,13 @@
+package com.spaceyx.space_tourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceTourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceTourismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spaceyx/space_tourism/SpaceTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceyx.space_tourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceTourismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
