6ed8ded87d8c29730a2b47ede3d5a83a95457f30
Carl Kone <carl.kone-takuefou@epita.fr>
2024-05-30 23:49:40 +0200

    feat: Add security configuration for login and authorization

---
+++ src/main/java/com/spaceyx/space_tourism/security/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package com.spaceyx.space_tourism.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Autowired
+    public DataSource dataSource;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .formLogin(login -> login
+                .failureForwardUrl("/")
+                .defaultSuccessUrl("/users")
+                .usernameParameter("username")
+                .passwordParameter("password")
+            )
+            .authorizeHttpRequests(authorization ->
+                authorization
+                    .requestMatchers("/login", "/error").permitAll()
+                    .requestMatchers("/users").hasRole("Technicien")
+                    .anyRequest().authenticated()
+            )
+            .build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        User.UserBuilder userBuilder = User.builder()
+            .passwordEncoder(pwd -> encoder().encode(pwd));
+        UserDetails technicien = userBuilder
+            .username("mrbricolage@spacyx.com")
+            .password("33raptor")
+            .roles("Technicien")
+            .build();
+        UserDetails planificateur = userBuilder
+            .username("voyagevoyage@spaceyx.com")
+            .password("ihaveaplan")
+            .roles("Planificateur")
+            .build();
+
+        UserDetails voyageur = userBuilder
+            .username("romain@mail.com")
+            .password("çavaaller")
+            .roles("Voyageur")
+            .build();
+        return new InMemoryUserDetailsManager(technicien, planificateur, voyageur);
+}
+
+@Bean
+public PasswordEncoder encoder(){
+    return new BCryptPasswordEncoder();
+}
+}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO utilisateur (email, password, role) VALUES ('mrbricolage@spacyx.com', '33raptor', 'TECHNICIEN');
-INSERT INTO utilisateur (email, password, role) VALUES ('voyagevoyage@spaceyx.com', 'ihaveaplan', 'PLANIFICATEUR');
-INSERT INTO utilisateur (email, password, role) VALUES ('romain@mail.com', 'çavaaller', 'VOYAGEUR');
